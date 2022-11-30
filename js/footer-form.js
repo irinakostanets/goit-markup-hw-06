5e0d4a1a57ac567b74d38a3079a4d1d9890b77f2
Irina Kostanets <irinakostanets22@gmail.com>
2022-11-30 14:21:25 +0200

    add checkbox changes

--- js/footer-form.js
+++
@@ -1,4 +0,0 @@
-function completeAndRedirect(){ 
-  var form = document.forms['myform'];
-text.innerHTML="Веденні вами дані: <br> І'мя : "+ form.elements.login.value + "<br> EMAIL : " + form.elements.email.value + "<br> ПОвідомлення : " + form.elements.messenger.value;
-}
